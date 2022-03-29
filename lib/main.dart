890cfb6f0f92e5b0369bd25f60f21f7df2b8a000
duncansu <duncan2001900220@gmail.com>
2022-03-29 09:44:32 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'app_bar.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    var appTitle=Text('flutter button');
+    var appBody=AppBody();
+    var appBar=AppBar(
+
+      title: appTitle,
+
+
+    );
+    var app=MaterialApp(
+
+      home: Scaffold(
+
+        appBar: appBar,
+        body: appBody,
+
+
+      ),
+
+
+    );
+
+    return app;
+  }
+}
+
